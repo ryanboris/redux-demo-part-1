2b74951abb083d669be862bdcd7a5f25dbf22ab8
ryanboris <ryan.boris@gmail.com>
2019-03-03 00:46:33 -0500

    adds demo reducer

--- src/index.js
+++ src/index.js
@@ -1,12 +1,56 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+/*
+  Pure functions:
+    return same results given same arguments
+    return is based only on incoming arguments
+    the function does not modify variables out of its scope
+    never produce side effects
+    
+*/
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function todos(state = [], action) {
+  if (action.type === 'ADD_TODO') {
+    return [...state, action.todo]
+  } else if (action.type === 'REMOVE_TODO') {
+    const index = state.findIndex()
+  }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  return state
+}
+
+function createStore() {
+  let state
+  let listeners = []
+  const getState = () => state
+  /*
+    Get the state
+    Listen for changes
+    Update state
+  */
+  const subscribe = listener => {
+    listeners = [...listeners, listener]
+    return () => {
+      listeners = listeners.filter(l => l !== listener)
+    }
+  }
+
+  const dispatch = action => {
+    state = todos(state, action)
+    listeners.map(listener => listener())
+  }
+
+  return {
+    getState,
+    subscribe,
+    dispatch
+  }
+}
+
+const store = createStore()
+store.dispatch({
+  type: 'ADD_TODO',
+  todo: {
+    id: 0,
+    name: 'Learn Redux',
+    complete: false
+  }
+})
