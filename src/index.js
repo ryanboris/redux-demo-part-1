af78e8e67a588cd1d5372d490ea0df792f6f225f
Ryan Boris <ryan.boris@gmail.com>
2019-03-04 11:41:23 -0500

    tidys up

--- src/index.js
+++ src/index.js
@@ -1,37 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
-import App from './components/App'
-import sep from './helpers/sepLogs'
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+// import { createStore } from 'redux'
+// import App from './components/App'
+// import sep from './helpers/sepLogs'
 
-const DEFAULT_SETTINGS = {
-  gameStarted: false,
-  instructionsExpanded: false
-}
-const rootReducer = (state = DEFAULT_SETTINGS, action) => {
-  console.log('state', state)
-  console.log('action', action)
-  if (action.type === 'SET_GAME_STARTED') {
-    return {
-      gameStarted: action.gameStarted,
-      instructionsExpanded: false
-    }
-  }
-  return state
-}
-const store = createStore(rootReducer)
-console.log('store', store)
-console.log('store.getState()', store.getState())
-store.subscribe(() =>
-  console.log('store.getState()', store.getState())
-)
-const action1 = { gameStarted: true, type: 'SET_GAME_STARTED' }
-store.dispatch(action1)
-console.log('store.getState()', store.getState())
-store.dispatch({ type: 'foo' })
+// const DEFAULT_SETTINGS = {
+//   gameStarted: false,
+//   instructionsExpanded: false
+// }
+// const rootReducer = (state = DEFAULT_SETTINGS, action) => {
 
-store.dispatch({ type: 'bar' })
+//   if (action.type === 'SET_GAME_STARTED') {
+//     return {
+//       gameStarted: action.gameStarted,
+//       instructionsExpanded: false
+//     }
+//   }
+//   return state
+// }
+// const store = createStore(rootReducer)
 
-console.log('store.getState()', store.getState())
+// store.subscribe(() =>
 
-ReactDOM.render(<App />, document.getElementById('root'))
+// )
+// const action1 = { gameStarted: true, type: 'SET_GAME_STARTED' }
+// store.dispatch(action1)
+
+// store.dispatch({ type: 'foo' })
+
+// store.dispatch({ type: 'bar' })
+
+// ReactDOM.render(<App />, document.getElementById('root'))
